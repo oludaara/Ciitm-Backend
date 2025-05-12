bface28e94ea9058b1fdec588d76c25049a4ed3b
shamoo53 <aliyushamsu16@gmail.com>
2025-05-12 20:50:54 +0100

    made the corrections

--- src/index.mjs
+++ src/index.mjs
@@ -89,12 +89,28 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 /**
  * @swagger
- * /api/users:
+ * /users:
  *   get:
  *     summary: Returns a list of users
+ *     description: Optional extended description
  *     responses:
  *       200:
  *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: The user ID.
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     description: The user's name.
+ *                     example: Leanne Graham
  */
 app.get('/users', (req, res) => {
   // Your route logic here
